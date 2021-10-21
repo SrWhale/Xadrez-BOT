64ec480890599894a372f3af82de0935662b09fa
SrWhale <contato.pauloideal@gmail.com>
2021-10-21 19:13:22 -0300

    15

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.login().then(async () => {
     client.connectdatabase();
     client.loadModules();
 
-    schedule('19 12 * * *', async () => {
+    schedule('15 19 * * *', async () => {
         const challenge = await get('https://lichess.org/api/puzzle/daily').then(res => res.data);
 
         const chess = new Chess({
